0a48dff371b9bfd803b539373009263ef932b61f
horodeski <geovana.horodeski06@gmail.com>
2023-01-15 23:50:46 -0300

    muito obrigado marco andre por existir

--- livraria/urls.py
+++ livraria/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from core import views
 
@@ -15,6 +19,8 @@ router.register(r'compras', views.CompraViewSet)
 urlpatterns = [
     # tirei as / pq tava dando erro
     path('admin', admin.site.urls),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('categorias-class', views.CategoriaView.as_view()),
     path('categorias-class/<int:id>', views.CategoriaView.as_view()),
     path('categorias-apiview', views.CategoriasList.as_view()),
